259fd6ab744355cb56c11cdc806049c2f200384e
Victor <victorrafaelravazi1234@gmail.com>
2026-03-03 01:34:49 -0300

    Generics em java

---
+++ src/generics/Caixa.java
@@ -0,0 +1,24 @@
+public class Caixa<T> {
+	private T coisaNaCaixa;
+
+	public void guardar(T coisa) {
+		this.coisaNaCaixa = coisa;
+	}
+
+	public T pegar() {
+		return coisaNaCaixa;
+	}
+
+	public static void main(String[] args) {
+		Caixa<String> caixaDeTexto = new Caixa<>();
+		caixaDeTexto.guardar("Olaa, Texto");
+		String texto = caixaDeTexto.pegar();
+		System.out.println(texto);
+		Caixa<Integer> caixaDeNumeros = new Caixa<>();
+		Integer numero = new Integer(4);
+		caixaDeNumeros.guardar(numero);
+
+		Integer valorResgatado = caixaDeNumeros.pegar();
+		System.out.println(valorResgatado);
+	}
+}
